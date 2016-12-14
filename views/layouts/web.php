005db02162d8a1995596cf3e97e7539074fc9c32
leiyabin <957919360@qq.com>
2016-12-13 16:36:06 -0800

    dev

--- views/layouts/web.php
+++ views/layouts/web.php
@@ -3,8 +3,8 @@
     <html>
     <head>
         <meta charset="utf-8">
-        <meta name="keywords" content="">
-        <meta name="description" content="">
+        <meta name="keywords" content="北京二手房出售,北京二手房买卖,北京买二手房,北京二手房网,北京二手房,通州二手房出售,通州二手房买卖,通州买二手房,通州二手房网,通州二手房">
+        <meta name="description" content="千氏地产网,买北京二手房就到千氏地产网,买通州二手房就到千氏地产网">
         <meta name="viewport" content="width=1200">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title><?= $this->title?></title>
