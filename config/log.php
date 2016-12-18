608bf8f4879a6c2dd2000cbe2029dbc01e9ef79b
leiyabin <957919360@qq.com>
2016-12-17 18:28:35 -0800

    优化log输出

--- config/log.php
+++ config/log.php
@@ -11,42 +11,49 @@ return [
         [
             'class'      => 'yii\log\FileTarget',
             'levels'     => ['error', 'info'],
+            'logVars'    => [],
             'categories' => ['application', 'request', 'response', 'rpc'],
             'logFile'    => '@app/runtime/logs/app.all.log',
         ],
         [
             'class'      => 'yii\log\FileTarget',
             'levels'     => ['info'],
+            'logVars'    => [],
             'categories' => ['request'],
             'logFile'    => '@app/runtime/logs/request.info.log'
         ],
         [
             'class'      => 'yii\log\FileTarget',
             'levels'     => ['error'],
+            'logVars'    => [],
             'categories' => ['request'],
             'logFile'    => '@app/runtime/logs/request.error.log'
         ],
         [
             'class'      => 'yii\log\FileTarget',
             'levels'     => ['info'],
+            'logVars'    => [],
             'categories' => ['response'],
             'logFile'    => '@app/runtime/logs/response.info.log'
         ],
         [
             'class'      => 'yii\log\FileTarget',
             'levels'     => ['error'],
+            'logVars'    => [],
             'categories' => ['response'],
             'logFile'    => '@app/runtime/logs/response.error.log'
         ],
         [
             'class'      => 'yii\log\FileTarget',
             'levels'     => ['error'],
+            'logVars'    => [],
             'categories' => ['rpc'],
             'logFile'    => '@app/runtime/logs/rpc.error.log'
         ],
         [
             'class'      => 'yii\log\FileTarget',
             'levels'     => ['info'],
+            'logVars'    => [],
             'categories' => ['rpc'],
             'logFile'    => '@app/runtime/logs/rpc.info.log'
         ],
